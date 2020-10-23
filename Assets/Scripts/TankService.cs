d012858c633a121b01cedefa2ef9061869a7468b
Varun Sharma <varunk22110@gmail.com>
2020-10-23 17:52:51 +0530

    Implemented tank movement. Needs some finishing.

---
+++ Assets/Scripts/TankService.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankService : MonoSingletonGeneric<TankService>
+{
+    public GameObject tankPlayer;
+    public Joystick joystick;
+    protected override void Awake()
+    {
+        base.Awake();
+        TankService.Instance.GetTank();
+    }
+
+    public TankController GetTank()
+    {
+        Instantiate(tankPlayer, Vector3.zero, Quaternion.identity);
+        TankController tankController = tankPlayer.GetComponent<TankController>();
+        tankController.joystick = joystick;
+        return tankController;
+    }
+
+    //public void GetTank()
+    //{
+    //    Instantiate(tankPlayer);
+    //}
+}
