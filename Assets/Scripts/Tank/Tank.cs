1f1416c2568a7cfa91d0e66057f1110ed321a369
Varun Sharma <varunk22110@gmail.com>
2020-12-01 19:07:32 +0530

    Restructured whole code.

--- Assets/Scripts/Tank/Tank.cs
+++ Assets/Scripts/Tank/Tank.cs
@@ -67,15 +67,13 @@ public class Tank : MonoBehaviour
         _currentHealth -= _damage;
         SetHealthUI();
 
-        if (_currentHealth <= 0f && !_dead) {
+        if (_currentHealth <= 0f) {
             OnDeath();
         }
 
     }
 
     private void OnDeath() {
-
-        _dead = true;
 
         _explosionParticles.transform.position = transform.position;
         _explosionParticles.gameObject.SetActive(true);
